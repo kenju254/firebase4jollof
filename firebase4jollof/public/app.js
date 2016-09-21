59f8b35a87d249ac326dd9514796df00b0d1fdce
Kenneth Kinyanjui <kenmbugua@gmail.com>
2016-09-21 03:46:25 +0200

    removing email provider

--- firebase4jollof/public/app.js
+++ firebase4jollof/public/app.js
@@ -30,7 +30,6 @@ var uiConfig = {
     },
     firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
   ],
   // Terms of service url.
   'tosUrl': 'https://www.google.com'
